ca1b795b7e5a2191efca31c8f665476dfeb35835
dickie <r.p.collins@sheffiel.ac.uk>
2018-07-13 16:59:22 +0100

    Adding in the Linear Kalman

--- Pressure_Surges.py
+++ Pressure_Surges.py
@@ -2,115 +2,135 @@ import pylab as pp
 import numpy as np
 import chaospy as cp
 import os
+from Transient_Calcs import *
+import pykalman as pk
 
+def demand_generator(filename,node,maxTime,dt,originalFlow,maxFlow,startTime,endTime):
+	"""
+	This function will create a simple transient demand control file for a single node
+	It accepts as arguments:
+	'filename' this is the required filename of the output file.
+	'node' this is the node name that you want to specify a control on
+	'maxTime' this is the simulation maximum time in seconds
+	'dt' the simulation dt
+	'originalFlow' the original flow at that node as specified in the SS simulation
+	'maxFlow' the flow you require after the flow change
+	
+	"""
+	times = np.arange(0,maxTime,dt)
+	demands = np.ones(times.shape)*originalFlow
+	demands[int(startTime/dt):int(endTime/dt)] = maxFlow
+	np.savetxt(filename,np.hstack((np.array(node),demands)),delimiter = ',')
 
 Directory = 'Projects/Pressure_Surges_Models/'
 FileName = '5Pipes.inp'
 maxTime = 30
 dt =0.01
+Wavespeed = 1000.
 Transient_Times = np.arange(0,maxTime,dt)
 
-distribution = cp.J(cp.Normal(1,0.4))#,cp.Normal(1,0.4),cp.Normal(1,0.4),cp.Normal(1,0.4),cp.Normal(1,0.4))
-demand_distribution = cp.Exponential(1.)
+#####
+# 	The input distributions
+#	Upstream Head =  N(20,0.1)  [m]
+#	Downstream Flow = N(0.001,0.0001)   [m3/s]
+#	Demand at node3 = Exp(0.0005)
+#	Roughness = N(1,0.4)
 
-Iterations = 1000
+Upstream = cp.Normal(20,0.1)
+Downstream = cp.Normal(0.001,0.0001)
+Demand = cp.Exponential(0.0005)
+Roughness = cp.J(cp.Normal(1,0.4),cp.Normal(1,0.4),cp.Normal(1,0.4),cp.Normal(1,0.4),cp.Normal(1,0.4))
 
-#samples = np.load(Directory + '5_pipes_varying_demand_samples.npy')
-#output = np.load(Directory + '5_pipes_varying_demand.npy')
+Distributions = cp.J(Upstream,Downstream,Demand,Roughness)
+#samples = Distributions.sample(1000000,'S')
+#np.save(Directory+'Transient_MC_samples.npy',samples)
+samples = np.load(Directory+'Transient_MC_samples.npy')
 
-samples = np.load(Directory + '5_pipes_varying_friction_all_same_samples.npy')
-output = np.load(Directory + '5_pipes_varying_friction_all_same.npy')
-
-####
-#	Plotting input distributions
-#pp.figure()
-#pp.hist(samples[0,:],density=True,color='k',bins=40,alpha=0.50,label='Samples')
-#d = np.linspace(min(samples[0,:]),max(samples[0,:]),1000)
-#pp.plot(d,cp.Normal(1,0.4).pdf(d),'k',label='Roughness PDF')
-#pp.xlabel('Roughness (mm)')
-#pp.ylabel('Frequency')
-#pp.show()
-
-Node = 4
-ConditionPoints = 100
-Order = 3
-
-polynomial_expansion = cp.orth_ttr(Order, distribution)
-foo_approx = cp.fit_regression(polynomial_expansion, samples[:ConditionPoints], output[:ConditionPoints,Node,:])
-
-coefs_kernal = cp.descriptives.misc.QoI_Dist(foo_approx,distribution)
+#
+#####		Monte Carlo Method
+#
+#
+Net = Import_EPANet_Geom(Directory+FileName)
+Net.open_epanet_file()
 
 
-#pp.figure(figsize=(10,6))
-#axs = []
-#axs.append(pp.subplot2grid((2,3),(0,0),colspan = 3))
-#axs.append(pp.subplot2grid((2,3),(1,0)))
-#axs.append(pp.subplot2grid((2,3),(1,1)))
-#axs.append(pp.subplot2grid((2,3),(1,2)))
+Iterations = 100000
+Output = np.zeros((Iterations,len(Net.nodes),Transient_Times.size))
 
-##axs[0].plot(Transient_Times,np.mean(output[:,Node,:].T,axis=1),'k--') 
-##axs[0].fill_between(Transient_Times,np.percentile(output[:,Node,:].T,5,axis=1),np.percentile(output[:,Node,:].T,95,axis=1),alpha = 0.5)
+ret,reservoir = epa.ENgetnodeindex(str(Net.nodes[-1].Name))
+ret,demand = epa.ENgetnodeindex(str(Net.nodes[1].Name))
+ret,downstream = epa.ENgetnodeindex(str(Net.nodes[-2].Name))
 
-##axs[0].plot(Transient_Times,cp.E(foo_approx,demand_distribution),'k')
-##axs[0].fill_between(Transient_Times,cp.Perc(foo_approx,5,demand_distribution)[0],cp.Perc(foo_approx,95,demand_distribution)[0],alpha = 0.5)
-#axs[0].plot(Transient_Times,np.mean(output[:,Node,:].T,axis=1)-cp.E(foo_approx,distribution),'k') 
+for i in range(Iterations):
+	print i
+	ret = epa.ENsetnodevalue(reservoir,epa.EN_ELEVATION,samples[0,i])
+	ret = epa.ENsetnodevalue(downstream,epa.EN_BASEDEMAND,samples[1,i]*1000.)
+	ret = epa.ENsetnodevalue(demand,epa.EN_BASEDEMAND,samples[2,i]*1000.)
+	for j in range(len(Net.pipes)):
+		ret,index = epa.ENgetlinkindex(str(Net.pipes[j].Name))
+		ret,epa.ENsetlinkvalue(index,epa.EN_ROUGHNESS,samples[3+j,i])
+		
+	Net.run_epanet_file()				## This function runs the SS EPAnet model	
+	Net.read_results_from_epanet()			## This function reads the SS EPAnet data into the transinet model
+	demand_generator(Directory+'5_pipes_driving_transient.csv',6,maxTime,dt,samples[1,i]*1000.,samples[1,i]*1000.+0.5,2,30)
+	Net.Assign_Emmiters_All()
+	Net.Constant_Wavespeed(Wavespeed)
+	Net.MOC_Initialisation(dt)
+	Net.Control_Input(Directory+'5_pipes_driving_transient.csv')
+	Net.MOC_Run(maxTime)
+	
+	count = 0					## The next few lines of code reads the output of the nodal pressures and feeds them into the Output array specified earlier.
+	for node in Net.nodes:
+		Output[i,count,:] = node.TranH
+		count+=1
+		
+np.save(Directory + '5_pipes_Monte_Carlo.npy',Output)
 
-##axs[0].plot(Transient_Times,np.percentile(output[:,Node,:].T,5,axis=1)-cp.Perc(foo_approx,5,demand_distribution)[0],'k:') 
-##axs[0].plot(Transient_Times,np.percentile(output[:,Node,:].T,95,axis=1)-cp.Perc(foo_approx,95,demand_distribution)[0],'k--') 
+#####		Linear Transient
+#
+#	Calculating the mean inputs
 
-#### Initial Time
-#a = axs[1].hist(output[:,Node,0],density=True,bins=100,alpha=0.50,color='k')
-#x = np.linspace(a[1][0],a[1][-1],1000)
-#axs[1].plot(x,coefs_kernal[0].pdf(x),color='k')
-#axs[1].set_title('t = 0 s')
+#Net = Import_EPANet_Geom(Directory+FileName)
+#Net.open_epanet_file()
+#ret,reservoir = epa.ENgetnodeindex(str(Net.nodes[-1].Name))
+#ret,demand = epa.ENgetnodeindex(str(Net.nodes[1].Name))
+#Net.nodes[1].demand = 0.0005
+#ret,downstream = epa.ENgetnodeindex(str(Net.nodes[-2].Name))
+#Net.nodes[-2].demand = 0.001
 
-#### Middle Time
-#a = axs[2].hist(output[:,Node,1500],density=True,bins=100,alpha=0.50,color='k')
-#x = np.linspace(a[1][0],a[1][-1],1000)
-#axs[2].plot(x,coefs_kernal[1500].pdf(x),color='k')
-#axs[2].set_title('t = 15 s')
+#ret = epa.ENsetnodevalue(reservoir,epa.EN_ELEVATION,20)
+#ret = epa.ENsetnodevalue(downstream,epa.EN_BASEDEMAND,0.001*1000.)
 
-#### Final Time
-#a = axs[3].hist(output[:,Node,-1],density=True,bins=100,alpha=0.50,color='k')
-#x = np.linspace(a[1][0],a[1][-1],1000)
-#axs[3].plot(x,coefs_kernal[-1].pdf(x),color='k')
-#axs[3].set_title('t = 30 s')
+#ret = epa.ENsetnodevalue(demand,epa.EN_BASEDEMAND,0.0005*1000.)
+#for j in range(len(Net.pipes)):
+#	ret,index = epa.ENgetlinkindex(str(Net.pipes[j].Name))
+#	ret,epa.ENsetlinkvalue(index,epa.EN_ROUGHNESS,1)
 
-#axs[0].set_title('Order: '+str(Order) + ', Collocation Points: ' + str(ConditionPoints))
+#Net.run_epanet_file()				## This function runs the SS EPAnet model	
+#Net.read_results_from_epanet()
+#Net.Constant_Wavespeed(Wavespeed)
+#Net.Initialise_Linear_Kalman(dt)
+#Net.P_Matrix = Net.P_Matrix.todense()
 
-#axs[0].set_xlim(0,30)
-#axs[0].set_xlabel('Time (s)')
-#axs[0].set_ylabel('Difference in Expected Head (m)')
+#Net.P_Matrix+=0.1
 
-#axs[1].set_ylabel('Frequency')
-#axs[1].set_xlabel('Head (m)')
-#axs[2].set_xlabel('Head (m)')
-#axs[3].set_xlabel('Head (m)')
-#pp.tight_layout()
-#pp.show()
+#Net.initial_BC_Uncertainty_Head(0.1)
+#kf = pk.KalmanFilter()
 
-pp.figure(figsize=(10,6))
+#kf.transition_matrices = Net.A_Matrix.todense()
 
-axs = []
-axs.append(pp.subplot2grid((1,2),(0,0)))
-axs.append(pp.subplot2grid((1,2),(0,1)))
-#axs.append(pp.subplot2grid((2,2),(1,0)))
-#axs.append(pp.subplot2grid((2,2),(1,1)))
+#kf.transition_covariance = Net.Q_Matrix.todense()
 
-axs[0].plot(Transient_Times,np.mean(output[:,0,:].T,axis=1),'k') 
-axs[0].fill_between(Transient_Times,np.percentile(output[:,0,:].T,5,axis=1),np.percentile(output[:,0,:].T,95,axis=1),color='k',alpha = 0.5)
+#kf.initial_state_mean = Net.X_Vector
+#kf.initial_state_covariance = Net.P_Matrix
 
-axs[1].plot(Transient_Times,np.mean(output[:,4,:].T,axis=1),'k') 
-axs[1].fill_between(Transient_Times,np.percentile(output[:,4,:].T,5,axis=1),np.percentile(output[:,4,:].T,95,axis=1),color='k',alpha = 0.5)
 
-#axs[2].plot(Transient_Times,cp.E(foo_approx,demand_distribution),'k')
-#axs[2].fill_between(Transient_Times,cp.Perc(foo_approx,5,demand_distribution)[0],cp.Perc(foo_approx,95,demand_distribution)[0],color='k',alpha = 0.5)
-
-axs[0].set_xlim(1.5,10)
-axs[1].set_xlim(1.5,10)
-
-axs[0].set_xlabel('Time (s)')
-axs[1].set_xlabel('Time (s)')
-axs[0].set_ylabel('Head (m)')
-pp.show()
-
+#StateOutput = np.zeros((Net.X_Vector.size,Transient_Times.size))
+#VarianceOutput = np.zeros((Net.X_Vector.size,Net.X_Vector.size,Transient_Times.size))
+#for i in range(0,Transient_Times.size):
+#	if i == int(2/dt):
+#		Net.X_Vector[2*Net.CPs+Net.nodes[-2].number]+=0.0005
+#		
+#	Net.X_Vector,Net.P_Matrix = kf.filter_update(Net.X_Vector,Net.P_Matrix)
+#	StateOutput[:,i] = Net.X_Vector
+#	VarianceOutput = Net.P_Matrix 
