495b1ded2a4be04141cfbda0b059a8f3ab39d51c
dickie <r.p.collins@sheffield.ac.uk>
2018-10-30 07:34:41 +0000

    more playing

--- UnsteadyFriction.py
+++ UnsteadyFriction.py
@@ -10,8 +10,8 @@ asigma = a0/200.
 g = 9.81		#gravity
 d0 = 0.05		#mean diameter
 dsigma = d0/200.	#std dev diameter
-f = 0.2			#friction
-f0 = 0.
+#f = 0.2			#friction
+f0 = 0.2
 fsigma = f0/200.
 Q0 = 0.001		#initial flow
 H0 = 10.		#upstream boundary
@@ -20,8 +20,8 @@ iters = 1
 epsilon = 0.85
 
 #Unsteady Friction Coeffs
-Kut = 0.0001
-Kux = 0.0004
+Kut = 0.0#01
+Kux = 0.0#01
 
 xPos = np.linspace(0,Length,NoNodes)
 dX = xPos[1]-xPos[0]
@@ -59,7 +59,7 @@ for i in range(iters):
 	H = np.zeros(len(xPos))
 	Q = np.ones(len(xPos))*Q0	## Initial Flow conditon
 	
-	signQdiffQ = np.ones(Q.shape)*np.sign(Q0)	## Initializing the QdiffQ vector
+	signQdiffQ = np.sign(np.gradient(Q)*Q)	## Initializing the QdiffQ vector
 	
 	#Setting the initial conditions of the pressure head
 	H[0] = H0
@@ -109,48 +109,46 @@ for i in range(iters):
 
 		QNew[1:-1] = (Cp - Cm)/(Bp + Bm)
 		HNew[1:-1] = (Cm*Bp + Bm*Cp)/(Bp+Bm)
-
+		
+		signQdiffQ = np.sign(np.gradient(Q)*Q)
 		###	Unsteady Friction Relationships
-		alpha_p = 2*a*(1+Kut) / (-signQdiffQ[:-1]*Kux + 2 + Kut)
-		alpha_m = 2*a*(1+Kut) / (-signQdiffQ[1:]*Kux - 2 - Kut)
+		alpha_p = 2*(1+Kut) / (-signQdiffQ[:-1]*Kux + 2 + Kut)
+		alpha_m = 2*(1+Kut) / (-signQdiffQ[1:]*Kux - 2 - Kut)
+#		alpha_p = 2*a / (-signQdiffQ[:-1]*Kux + 2 + Kut)
+#		alpha_m = 2*a / (-signQdiffQ[1:]*Kux - 2 - Kut)
 		
 		
+		Hp = H[1:-1] - a[1:]*alpha_p[1:] * dT/dX *( H[1:-1] - H[:-2])
+		Hm = H[1:-1] + a[:-1]*alpha_m[:-1] * dT/dX *( H[1:-1] - H[2:])
 		
-		Hp = H[1:-1] - alpha_p[1:] * dT/dX *( H[1:-1] - H[:-2])
-		Hm = H[1:-1] + alpha_m[:-1] * dT/dX *( H[1:-1] - H[2:])
-		
-		Qp = Q[1:-1] - alpha_p[1:] * dT/dX *( Q[1:-1] - Q[:-2])
-		Qm = Q[1:-1] + alpha_m[:-1] * dT/dX *( Q[1:-1] - Q[2:])
+		Qp = Q[1:-1] - a[1:]*alpha_p[1:] * dT/dX *( Q[1:-1] - Q[:-2])
+		Qm = Q[1:-1] + a[:-1]*alpha_m[:-1] * dT/dX *( Q[1:-1] - Q[2:])
 		
 		Cap = (g*pipeA / a) / alpha_p
-		Cpp = Qp + Cap[:-1] * Hp  - f[:-1] * Qp * np.abs(Qp) / (2*pipeD[:-1]*(1+Kut))
+		Cpp = Qp + Cap[:-1] * Hp  - f[:-1] * Qp * np.abs(Qp) * dT/ (2*pipeD[:-1]*(1+Kut))
 		
 		Cam = (g*pipeA / a) / alpha_m
-		Cmm = Qm + Cam[1:]*Hm - f[1:] * Qm * np.abs(Qm) / (2*pipeD[1:]*(1+Kut))
-		
-		
-		
-		#QNew[1:-1] = (Cp[1:] - Cm[:-1])/(Bp[1:] + Bm[:-1])
+		Cmm = Qm + Cam[1:]*Hm - f[1:] * Qm * np.abs(Qm) *dT/ (2*pipeD[1:]*(1+Kut))
+	
 		HNew[1:-1] = (Cpp - Cmm)/(Cap[:-1] - Cam[:1])
-		#HNew[1:-1] = (Cm[:-1]*Bp[1:] + Bm[:-1]*Cp[1:])/(Bp[1:]+Bm[:-1])
 		QNew[1:-1] = (Cmm*Cap[:-1] - Cpp*Cam[:1])/(Cap[:-1]-Cam[:1])
 		
-		
+		sadasd
 		
 		### Boundary Conditions
-		if t > 0.5:
-#			if QBC > 0:
-#				QBC = 0.0005
-#				#QBC = QBC - 0.00001#0.0005
-#			else:
-#				QBC = 0
-				
-			QBC = 0.002
-		if t > 5:
-			QBC = 0.001
-		if t <= 0.5:
-			QBC = Q0
-			
+#		if t > 0.5:
+##			if QBC > 0:
+##				QBC = 0.0005
+##				#QBC = QBC - 0.00001#0.0005
+##			else:
+##				QBC = 0
+#				
+#			QBC = 0.002
+#		if t > 5:
+#			QBC = 0.001
+#		if t <= 0.5:
+#			QBC = Q0
+		QBC = Q0	
 			
 			
 		##
@@ -165,29 +163,32 @@ for i in range(iters):
 		QNew[0] = (H[0] - Cm) / Bm
 		
 		### Unsteady Friction Versions of the Boundary Conditions
-		alpha_p = 2*a[-1]*(1+Kut) / (-signQdiffQ[-1]*Kux + 2 + Kut)
-		alpha_m = 2*a[0]*(1+Kut) / (-signQdiffQ[0]*Kux - 2 - Kut)
+		alpha_p = 2*(1+Kut) / (-signQdiffQ[-1]*Kux + 2 + Kut)
+		alpha_m = 2*(1+Kut) / (-signQdiffQ[0]*Kux - 2 - Kut)
+#		alpha_p = 2*a[-1] / (-signQdiffQ[-1]*Kux + 2 + Kut)
+#		alpha_m = 2*a[0] / (-signQdiffQ[0]*Kux - 2 - Kut)
 		
-		Hp = H[-1] - alpha_p * dT/dX *( H[-1] - H[-2])
-		Hm = H[0] + alpha_m * dT/dX *( H[0] - H[1])
-		Qp = Q[-1] - alpha_p * dT/dX *( Q[-1] - Q[-2])
-		Qm = Q[0] + alpha_m * dT/dX *( Q[0] - Q[1])
+		
+		Hp = H[-1] -a[-1]*alpha_p * dT/dX *( H[-1] - H[-2])
+		Hm = H[0] + a[-1]*alpha_m * dT/dX *( H[0] - H[1])
+		Qp = Q[-1] - a[0]*alpha_p * dT/dX *( Q[-1] - Q[-2])
+		Qm = Q[0] + a[0]*alpha_m * dT/dX *( Q[0] - Q[1])
 		
 		Cap = (g*pipeA[-1] / a[-1]) / alpha_p
-		Cpp = Qp + Cap * Hp  - f[-1] * Qp * np.abs(Qp) / (2*pipeD[-1]*(1+Kut))
+		Cpp = Qp + Cap * Hp  - f[-1] * Qp * np.abs(Qp) *dT/ (2*pipeD[-1]*(1+Kut))
 		
 		Cam = (g*pipeA[0] / a[0]) / alpha_m
-		Cmm = Qm + Cam*Hm - f[1] * Qm * np.abs(Qm) / (2*pipeD[1]*(1+Kut))
+		Cmm = Qm + Cam*Hm - f[0] * Qm * np.abs(Qm) *dT/ (2*pipeD[0]*(1+Kut))
 		
 		
 		HNew[0] = H0
-		QNew[0] = Cmm + Cam*HNew[0]
-		
+		QNew[0] = Cmm + Cam*H0
+				
 		QNew[-1] = QBC#(Cp - H[-1]) / Bp
 		HNew[-1] = (QNew[-1] - Cpp)/(-Cap)
 				
 		
-		signQdiffQ = np.sign(QNew*(QNew-Q))
+		
 		
 		H = np.copy(HNew)
 		Q = np.copy(QNew)
@@ -202,6 +203,7 @@ for i in range(iters):
 	QData.append(QArray)
 	HData.append(HArray)
 	#hList.append(H)
+	pp.plot(time,HArray[:,0])
 	pp.plot(time,HArray[:,NoNodes/10])
 	pp.plot(time,HArray[:,NoNodes/2])
 	pp.plot(time,HArray[:,-1])
